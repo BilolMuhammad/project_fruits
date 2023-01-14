510c2b00a10d65157da80a05aecb1f95024de172
PRESTIGE <mamatqulov@gmail.com>
2023-01-14 19:18:26 +0500

    o3

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -1,4 +1,4 @@
-def get_expensive_fruit(data:str)->str:
+def get_expensive_fruit(data: str) -> str:
     """
     This function returns the name of the most expensive fruit in the list
 
@@ -8,6 +8,20 @@ def get_expensive_fruit(data:str)->str:
         name of the most expensive fruit
     """
     # your code here
-    return  
+    data = open(data).read().split('\n')[1:]
+    num_list = []
+    for n in data:
+        num_list.append(float(n.split(',')[1]))
+
+    max = num_list[0]
+    for n in num_list:
+        if n > max:
+            max = n
+    ans = ''
+    for d in data:
+        if float(d.split(',')[1]) == max:
+            ans = d.split(',')[0]
+    return ans
 
 
+print(get_expensive_fruit('fruits.csv'))
