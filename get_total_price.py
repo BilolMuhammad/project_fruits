0576bd2dd7653024efd68f38d6d6c1c5009cc197
PRESTIGE <mamatqulov@gmail.com>
2023-01-14 19:19:37 +0500

    o5

--- get_total_price.py
+++ get_total_price.py
@@ -1,4 +1,7 @@
-def get_total_price(data:str)->float:
+import csv
+
+
+def get_total_price(data: str) -> float:
     """
     This function returns the total price of the fruits in the list
 
@@ -7,6 +10,13 @@ def get_total_price(data:str)->float:
     returns:
         list of fruits total price
     """
-    return
+    file = open(data)
+    reader = csv.reader(file)
+    next(reader)
+    sum = 0
+    for n in reader:
+        sum += float(n[1])
+    return sum
 
-    
+
+print(get_total_price('fruits.csv'))
