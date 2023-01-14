9edbe9d13350a7e3c4e39a89874703897d906d94
PRESTIGE <mamatqulov@gmail.com>
2023-01-14 19:17:18 +0500

    o2

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -1,4 +1,4 @@
-def get_cheapest_fruit(data:str)->str:
+def get_cheapest_fruit(data: str) -> str:
     """
     This function returns the name of the cheapest fruit in the list
 
@@ -9,3 +9,20 @@ def get_cheapest_fruit(data:str)->str:
     """
     # your code here
     pass
+    reader = open(data).read().split('\n')[1:]
+    num_list = []
+    for r in reader:
+        num_list.append(float(r.split(',')[1]))
+
+    min = num_list[0]
+    for n in num_list:
+        if n < min:
+            min = n
+    ans = ''
+    for r in reader:
+        if float(r.split(',')[1]) == min:
+            ans = r.split(',')[0]
+    return ans
+
+
+print(get_cheapest_fruit('fruits.csv'))
